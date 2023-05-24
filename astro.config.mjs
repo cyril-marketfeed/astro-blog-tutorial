3678221a5c41c12c0b654e744a236249b808478f
Cyril Joy <115976437+cyril-marketfeed@users.noreply.github.com>
2023-05-24 18:21:45 +0530

    Add Prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact(), prefetch()]
 });
